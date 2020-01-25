LineageOS/android_device_google_bonito

bonito: Add proprietary file listing
Change-Id: Id399faa404207ce018f0f0187c9576b61f5d4dc9

extract-files.sh
@@ -1,0 +1,70 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+
+DEVICE=bonito
+VENDOR=google
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+LINEAGE_ROOT="$MY_DIR"/../../..
+
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -n | --no-cleanup )     CLEAN_VENDOR=false
+                                ;;
+        -s | --section )        shift
+                                SECTION=$1
+                                CLEAN_VENDOR=false
+                                ;;
+        --sargo )               shift
+                                SARGO_SRC=$1
+                                ;;
+        --bonito )              shift
+                                BONITO_SRC=$1
+                                ;;
+    esac
+    shift
+done
+
+if [ -z "$SRC" ]; then
+    SRC=adb
+fi
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
+
+extract "$MY_DIR"/bonito-proprietary-files.txt "$BONITO_SRC" "$SECTION"
+
+# Reinitialize the helper for sargo
+DEVICE=sargo
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
+
+extract "$MY_DIR"/sargo-proprietary-files.txt "$SARGO_SRC" "$SECTION"
+
+"$MY_DIR"/setup-makefiles.sh

setup-makefiles.sh
@@ -1,0 +1,69 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+
+DEVICE=bonito
+DEVICE_COMMON=bonito
+VENDOR=google
+
+INITIAL_COPYRIGHT_YEAR=2019
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+LINEAGE_ROOT="$MY_DIR"/../../..
+
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" true
+
+# Copyright headers and guards
+write_headers "bonito sargo"
+
+write_makefiles "$MY_DIR"/bonito-proprietary-files.txt true
+
+cat << EOF >> "$ANDROIDMK"
+
+EOF
+
+# Finish bonito
+write_footers
+
+# Reinitialize the helper
+DEVICE=sargo
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+
+# Copyright headers and guards
+write_headers
+
+write_makefiles "$MY_DIR"/sargo-proprietary-files.txt true
+
+cat << EOF >> "$ANDROIDMK"
+
+EOF
+
+# Finish
+write_footers

gee-one/device_google_bonito

extract-files.sh
@@ -1,0 +1,70 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+
+DEVICE=bonito
+VENDOR=google
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+GAHS_ROOT="$MY_DIR"/../../..
+
+HELPER="$GAHS_ROOT"/vendor/gahs/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -n | --no-cleanup )     CLEAN_VENDOR=false
+                                ;;
+        -s | --section )        shift
+                                SECTION=$1
+                                CLEAN_VENDOR=false
+                                ;;
+        --sargo )               shift
+                                SARGO_SRC=$1
+                                ;;
+        --bonito )              shift
+                                BONITO_SRC=$1
+                                ;;
+    esac
+    shift
+done
+
+if [ -z "$SRC" ]; then
+    SRC=adb
+fi
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$GAHS_ROOT" false "$CLEAN_VENDOR"
+
+extract "$MY_DIR"/bonito-proprietary-files.txt "$BONITO_SRC" "$SECTION"
+
+# Reinitialize the helper for sargo
+DEVICE=sargo
+setup_vendor "$DEVICE" "$VENDOR" "$GAHS_ROOT" false "$CLEAN_VENDOR"
+
+extract "$MY_DIR"/sargo-proprietary-files.txt "$SARGO_SRC" "$SECTION"
+
+"$MY_DIR"/setup-makefiles.sh

setup-makefiles.sh
@@ -1,0 +1,69 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+
+DEVICE=bonito
+DEVICE_COMMON=bonito
+VENDOR=google
+
+INITIAL_COPYRIGHT_YEAR=2019
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+GAHS_ROOT="$MY_DIR"/../../..
+
+HELPER="$GAHS_ROOT"/vendor/gahs/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$GAHS_ROOT" true
+
+# Copyright headers and guards
+write_headers "bonito sargo"
+
+write_makefiles "$MY_DIR"/bonito-proprietary-files.txt true
+
+cat << EOF >> "$ANDROIDMK"
+
+EOF
+
+# Finish bonito
+write_footers
+
+# Reinitialize the helper
+DEVICE=sargo
+setup_vendor "$DEVICE" "$VENDOR" "$GAHS_ROOT"
+
+# Copyright headers and guards
+write_headers
+
+write_makefiles "$MY_DIR"/sargo-proprietary-files.txt true
+
+cat << EOF >> "$ANDROIDMK"
+
+EOF
+
+# Finish
+write_footers
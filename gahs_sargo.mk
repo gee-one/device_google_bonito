584931678b6f64eefe9c1f22efe91bda9a9201e5
Rashed Abdel-Tawab <rashed@linux.com>
2019-05-14 00:36:02 -0700

    bonito: Add proprietary file listing
    Change-Id: Id399faa404207ce018f0f0187c9576b61f5d4dc9

--- gahs_sargo.mk
+++ gahs_sargo.mk
@@ -17,6 +17,9 @@
 $(call inherit-product, device/google/bonito/aosp_sargo.mk)
 $(call inherit-product, vendor/gahs/config/common.mk)
 $(call inherit-product, device/google/bonito/device-gahs.mk)
+# Include sargo before bonito to use sargo versions of blobs if they exist
+$(call inherit-product-if-exists, vendor/google/sargo/sargo-vendor.mk)
+$(call inherit-product-if-exists, vendor/google/bonito/bonito-vendor.mk)
 
 PRODUCT_NAME := gahs_sargo
 PRODUCT_BRAND := google
